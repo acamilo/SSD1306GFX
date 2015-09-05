cc89cca32b8c2524977e92b9b437049024994dc3
Alex Camilo <alex.camilo@gmail.com>
2015-09-05 19:00:48 -0400

    suck new sources into makefile

--- main.mk
+++ main.mk
@@ -2,7 +2,10 @@ TARGET := main
 
 SOURCES := \
     main.c \
-    src/screen.c
+    src/screen.c \
+	src/sdl_funcs.c\
+	src/gfx.c \
+	fonts/fonts.c
 
 CXXFLAGS := -g -O0 -Wall -pipe -framework SDL2 -framework GLUT -framework OpenGL 
 
